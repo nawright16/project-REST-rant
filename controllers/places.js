c2c950620304d84e24e7f035e4c331cf7fb0e34e
Nick Wright <nwright@egnyte.com>
2023-02-24 10:41:42 -0500

    adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
+// NEW
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
 //GET /places
 router.get('/', (req, res) => {
     res.render('places/index', { places })

4d252c9fc39bf0950d7b06afea117fda59c374b1
Nick Wright <nwright@egnyte.com>
2023-03-06 14:59:27 -0500

    added validations

--- controllers/places.js
+++ controllers/places.js
@@ -5,26 +5,37 @@ const db = require('../models')
 // find
 router.get('/', (req, res) => {
   db.Place.find()
-  .then((places) => {
-    res.render('places/index', { places })
-  })
-  .catch(err => {
-    console.log(err)
-    res.render('error404')
-  })
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error404')
+    })
 })
 
 // create
 router.post('/', (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
+    .then(() => {
       res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
+    })
+    .catch(err => {
+      if (err && err.name == 'ValidationError') {
+        let message = 'Validation Error: '
+        for (var field in err.errors) {
+            message += `${field} was ${err.errors[field].value}. `
+            message += `${err.errors[field].message}`
+        }
+        console.log('Validation error message', message)
+        res.render('places/new', { message })
+    }
+    else {
+        res.render('error404')
+    }
+    })
 })
+
 
 //
 router.get('/new', (req, res) => {
@@ -33,13 +44,13 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-  .then(place => {
+    .then(place => {
       res.render('places/show', { place })
-  })
-  .catch(err => {
+    })
+    .catch(err => {
       console.log('err', err)
       res.render('error404')
-  })
+    })
 })
 
 
@@ -61,7 +72,7 @@ router.post('/:id/rant', (req, res) => {
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+  res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
